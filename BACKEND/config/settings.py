739fa2959af9c8a4e308cc099d55859d4036bc75
Oliver <venturavalverdeo@gmail.com>
2026-02-05 08:39:07 -0400

    act

--- BACKEND/config/settings.py
+++ BACKEND/config/settings.py
@@ -24,7 +24,7 @@ DEBUG = "RENDER" not in os.environ  # En Render, DEBUG=False
 # -----------------------------
 # Hosts / CORS / CSRF
 # -----------------------------
-ALLOWED_HOSTS = ["jardinbackend-a5p0.onrender.com", "localhost", "127.0.0.1", "grieving-elene-jardindelasdelicias-354ed852.koyeb.app"]
+ALLOWED_HOSTS = ["jardinbackend-a5p0.onrender.com", "localhost", "127.0.0.1", "grieving-elene-jardindelasdelicias-354ed852.koyeb.app", ".koyeb.app"]
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
